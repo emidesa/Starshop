73adfa9c2be8e22a57efe847785c84ee00bf275f
Emeline De sa <emeline.desa@outlook.fr>
2025-06-17 18:18:34 +0200

    les services

--- src/Controller/mainController.php
+++ src/Controller/mainController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-
+use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,13 +10,15 @@ use Symfony\Component\Routing\Attribute\Route;
 class mainController extends AbstractController
 {
     #[Route('/')]
-    public function homepage(): Response
+    public function homepage(StarshipRepository $starshipRepository): Response
 
     {
-        $starshipCount = 457;
+        $ships = $starshipRepository->findAll();
+        $myShip = $ships[array_rand($ships)];
 
         return $this->render('main/homepage.html.twig', [
-            'numberOfStarships' => $starshipCount,
+            'myShip' => $myShip,
+            'ships' => $ships
         ]);
     }
 }
---
+++ src/Repository/StarshipRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repository;
+
+use App\Model\Starship;
+use Psr\Log\LoggerInterface;
+
+class StarshipRepository
+{
+
+    public function __construct(private LoggerInterface $logger)
+    {
+        $this->logger->info ('Starship collection retrieved');
+
+    }
+    public function findAll(): array
+
+    {
+
+    return [
+    new Starship(
+        1,
+        'USS LeafyCruiser (NCC-0001)',
+        'Garden',
+        'Jean-Luc Pickles',
+        'taken over by Q',
+
+    ),
+
+    new Starship(
+        2,
+        'USS Espresso (NCC-1234-C)',
+        'Latte',
+        'James T. Quick!',
+        'repaired',
+    ),
+
+    new Starship(
+        3,
+         'USS Wanderlust (NCC-2024-W)',
+         'Delta Tourist',
+         'Kathryn Journeyway',
+         'under construction',
+    ),
+    
+];
+}
+}
